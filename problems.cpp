8b322bcfb712ed64332423595cea084b65ee3ddc
u.jumaqulov <u.jumaqulov@newuu.uz>
2024-09-17 12:44:06 +0500

    1

---
+++ problems.cpp
@@ -0,0 +1,113 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+
+//    //problem1
+//    int n;
+//    cin >> n;
+//    cout << (n>=0 ? "Positive\n": "Negative\n");
+
+
+//    //problem3
+//    int a,b,c;
+//    cin >> a >> b >> c;
+//    cout << "Largest is " << (a > b? (a > c? a:c): (b > c? b:c) );
+
+//    //problem4
+//    int a,b;
+//    cin >> a >> b;
+//    if (a % b == 0){
+//        cout << a << " is divisible by " << b;
+//    }else{
+//        cout << a << " is not divisible by " << b;
+//    }
+
+
+//    //problem5
+//    int n;
+//    cin >> n;
+//    if (n % 4 ==0){
+//        if (n % 100 ==0){
+//            if (n % 400 ==0){
+//                cout << n << " is a lap year ";
+//            }else{
+//                cout << n << " is not a lap year ";
+//            }
+//        }else{
+//            cout << n << " is a lap year ";
+//        }
+//    }else{
+//        cout << n << " is not a lap year ";
+//    }
+
+
+//    //problem6
+//    float a,b,c;
+//    cin >> a >> b >> c;
+//    double d = b*b - 4*a*c;
+//    if (d>0){
+//        double x1 = (-b + pow(d,1/2.0))/(2*a);
+//        double x2 = (-b - pow(d,1/2.0))/(2*a);
+//        cout << "x1="<<x1 << endl;
+//        cout << "x2="<<x2;
+//    }else if (d==0){
+//        double x1 = (-b/(2*a));
+//    }else{
+//        cout << "No solution";
+//
+//    }
+
+
+//    //problem7
+//    int a,b,c;
+//    string q = "The triangle is valid";
+//    string q2 = "The triangle is not valid";
+//    cin >> a >> b >> c;
+//
+//    cout << (a+b>c? (a+c>b? (b+c>a? q:q2):q2):q2)<< endl;
+
+
+//    //problem8
+//    char a;
+//    cin >> a;
+//    int c=a;
+//    if ((90>=c) and (c>=65)){
+//        cout << "Uppercase alphabet";
+//    }else if ((122>=c) and (c>=97)){
+//        cout << "lowercase alphabet";
+//
+//    }else{
+//        cout << "It is not alphabet";
+//    }
+
+//    //problem9
+//    float a,b;
+//    cin >> a >> b;
+//    float c = pow((a*a+ b*b),.5);
+//    cout << (c<5? "Point is in circle":"Point is not in circle");
+
+
+
+//    //problem10
+//    float a,b,c,d;
+//    cin >> a >> b;
+//    cin >> c >> d;
+//    float q1 = a / b;
+//    float q2= c / d;
+//    if (q1>q2){
+//        cout << "Package 1 has a better price";
+//    }else if(q2>q1){
+//        cout << "Package 2 has a better price";
+//    }else{
+//        cout << "Two packages have a the same price";
+//    }
+
+
+
+
+
+
+
+    return 0;
+}
