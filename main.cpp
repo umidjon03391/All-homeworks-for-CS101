1d35f4b3ed967fed1aae5ae1d1b05b38ede65f81
u.jumaqulov <u.jumaqulov@newuu.uz>
2024-09-24 13:00:49 +0500

    1

---
+++ main.cpp
@@ -0,0 +1,118 @@
+#include <iostream>
+using namespace std;
+int main() {
+//    //problem1
+//    for (int i = 1; i<=10; i++ ){
+//        cout << i<<" ";
+//    }
+
+//    //problem2
+//    int sum=0;
+//    for (int i=1; i<=10; i++){
+//        cout << i << " ";
+//        sum += i;
+//    }
+
+ //   //problem3
+//    int i=0;
+//    while(i++<100){
+//        if (i % 5 !=0)
+//            cout << i << " ";
+//    }
+
+//    //problem4
+//    int n;
+//    int i = 1;
+//    cin >> n;
+//    while (i <= n){
+//        if ( i % 10 ==0){
+//            cout << i << " ";
+//        }
+//        i+=1;
+//    }
+
+
+//    //problem5
+//    for (int i =65; i<=90; i++ ){
+//        if (i%5==4) {
+//            cout << char(i) << " ";
+//            cout << endl;
+//        }
+//        else
+//            cout << char(i) << " ";
+//    }
+
+
+
+//    //problem6
+//    int n;
+//    float hours=0,ch=0 ;
+//    cin >> n;
+//    for (int i=1; i <= n; i++){
+//        float a,b;
+//        cin >> a>> b;
+//        hours+=a;
+//        ch=ch+a*b;
+//    }
+//    cout<< "Your total GPA is " << ch/hours;
+
+
+//    //problem7
+//    int a;
+//    bool w=true;
+//    cin>>a;
+//    for(int i=2; i<a; i++){
+//        if (a%i==0) {
+//            w=0;
+//            break;
+//        }else{
+//            w=true;
+//        }
+//    }
+//    if (w){
+//        cout << "prime";
+//    }else{
+//        cout<<"not prime";
+//    }
+
+
+//    //problem8
+//    int n;
+//    cin >> n;
+//    for (int i = 1 ; i <= n; i++ ){
+//        cout << i <<"*" << i << "=" << i*i << endl;
+//    }
+//
+
+//    //problem9
+//    int max =0;
+//    while (true){
+//        int a;
+//        cin >> a;
+//        if (a==0){
+//            break;
+//        }
+//        else if (a>max){
+//            max=a;
+//        }
+//    }
+//    cout << max;
+
+
+    //problem10
+    //    int max =0;
+//    while (true){
+//        int a;
+//        cin >> a;
+//        if (a==0){
+//            break;
+//        }
+//        else if (a>max){
+//            max=a;
+//        }
+//    }
+//    cout << max;
+
+
+    return 0;
+}
